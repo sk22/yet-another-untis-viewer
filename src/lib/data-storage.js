e0b1066ce081b49a1c670ecfe21faaa2278f7995
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-05-05 22:55:37 +0200

    Replaced sessionStorage with localStorage

--- src/lib/data-storage.js
+++ src/lib/data-storage.js
@@ -1,9 +1,9 @@
 export const save = state => {
-  sessionStorage.setItem('state', JSON.stringify(state))
+  localStorage.setItem('state', JSON.stringify(state))
 }
 
 export const load = () => {
-  const string = sessionStorage.getItem('state')
+  const string = localStorage.getItem('state')
   if (string === 'undefined') return null
   return JSON.parse(string)
 }
